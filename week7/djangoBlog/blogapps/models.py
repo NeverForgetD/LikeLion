48453cc1726fdb705204552da441a1d91bdd9034
JY_xps <0327max@gmail.com>
2023-05-16 20:08:17 +0900

    add week7 file / field 'movie blog' into 'game blog'

---
+++ week7/djangoBlog/blogapps/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    playtime = models.FloatField()
+    review = models.TextField()
+
+    def summary(self):
+        return self.review[:10]
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.comment
---
+++ week7/djangoBlog/blogapps/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Game, Comment
+from django.utils import timezone
+
+# Create your views here.
+def home(request):
+    games = Game.objects.all()
+    count = len(games)
+    ctx = {
+        'games': games,
+        'count' : count,
+    }
+    return render(request, 'home.html', ctx)
+
+def detail(request, game_id):
+    game = get_object_or_404(Game, pk=game_id)
+    return render(request, 'detail.html', {'game':game})
+
+def create(request):
+    if request.method == 'POST':
+        game = Game()
+        game.title = request.POST['title']
+        game.playtime = float(request.POST['playtime'])
+        game.review = request.POST['review']
+        game.save()
+        return redirect('detail', game.id)
+    return render(request, 'create.html')
+
+def comment(request, game_id):
+    if request.method == 'POST':
+        new_comment = Comment()
+        new_comment.game = get_object_or_404(Game, pk=game_id)
+        new_comment.comment = request.POST['comment']
+        new_comment.created_at = timezone.now()
+        new_comment.save()
+    return redirect('detail', game_id)
